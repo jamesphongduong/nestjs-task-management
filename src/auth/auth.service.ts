d80534bcf333ba71e8778aff64e4803b560d624e
James <james.duong93@gmail.com>
2020-07-06 12:30:10 +1000

    signing jwt token upon auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,19 +2,24 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentials: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentials);
   }
 
-  async signIn(authCredentials: AuthCredentialsDto) {
+  async signIn(
+    authCredentials: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const username = await this.userRepository.validateUserPassword(
       authCredentials,
     );
@@ -22,5 +27,10 @@ export class AuthService {
     if (!username) {
       throw new UnauthorizedException('Invalid credentials');
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }

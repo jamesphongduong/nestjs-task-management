3684c0f06f00f272dd41b755c304398bc1b79fe9
James <james.duong93@gmail.com>
2020-07-09 12:50:05 +1000

    import fixes

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,42 @@
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TaskRepository } from './task.repository';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
+
+const mockUser = { username: 'Test user' };
+
+const mockTaskRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+describe('TasksService', () => {
+  let tasksService;
+  let taskRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mockTaskRepository },
+      ],
+    }).compile();
+
+    tasksService = await module.get<TasksService>(TasksService);
+    taskRepository = await module.get<TaskRepository>(TaskRepository);
+  });
+
+  describe('getTasks', () => {
+    it('gets all tasks from the repository', () => {
+      expect(taskRepository.getTasks).not.toHaveBeenCalled();
+
+      const filters: GetTasksFilterDto = {
+        status: TaskStatus.IN_PROGRESS,
+        search: 'some search query',
+      };
+
+      tasksService.getTasks(filters, mockUser);
+      expect(taskRepository.getTasks).toHaveBeenCalled();
+    });
+  });
+});

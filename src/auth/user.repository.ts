3bef604c8a195f470ce712fd5a787f42db42352e
James <james.duong93@gmail.com>
2020-07-11 14:41:35 +1000

    testing userrepo

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,14 +5,14 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = new User();
+    const user = this.create();
     user.username = username;
     user.salt = await bcrypt.genSalt();
     user.password = await this.hashPassword(password, user.salt);
